1a7811449fc59b604d11288b94037cdf022ab88b
dehuli <121349692+hamigualisingl@users.noreply.github.com>
2024-08-01 17:00:21 +0800

    Update train_V2.py

--- train_V2.py
+++ train_V2.py
@@ -12,9 +12,8 @@ from torch.utils.tensorboard import SummaryWriter
 from torch import distributed as dist
 
 import dnnlib
-from modelv2 import Stled_vae_vq_V2
-from vq_model import VQ_models
- 
+
+from modelV2 import  VQVAE_Transformer_vit_sd3_hug_4096#VQVAE_Transformer_expert_vit_no_qunat_hug_32768
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch-size", type=int, default=32)
 parser.add_argument("--beta1", type=float, default=0.9, help="adamw")
@@ -69,7 +68,7 @@ def main():
         summary_writer = SummaryWriter(os.path.join(args.output, "tensorboard"))
     else:
         summary_writer = None
-    model_alip = Stled_vae_vq_V2(width=1024, layers=24, heads=16, mlp_ratio=4.0,emb_dim=128)
+    model_alip = VQVAE_Transformer_vit_sd3_hug_4096(width=1024, layers=24, heads=16, mlp_ratio=4.0,emb_dim=128)
     model_alip.cuda()
     model_alip = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model_alip)
     # static =torch.load('/mnt/data/user/lidehu/vae/ALIP/test.pt', map_location=map_location)
@@ -79,15 +78,7 @@ def main():
         bucket_cap_mb=32,
         find_unused_parameters=True,
         static_graph=True)
-    #代理模型llamagen   https://github.com/FoundationVision/LlamaGen  #感谢大佬的工作！选取在LAION COCO (50M) + internal data (10M)训练的版本
-    vq_model = VQ_models['VQ-16'](
-        codebook_size=16384,
-        codebook_embed_dim=8).eval().cuda()
-    checkpoint = torch.load('/mnt/data/user/lidehu/vae/LlamaGen/vq_ds16_t2i.pt', map_location=map_location)
-    model_weight = checkpoint["model"]
-    vq_model.load_state_dict(model_weight)
-    ######测试 代理模型     
-    # train_loader = dali_dataloader(args)
+    #加载模型权重  需要指定加载设备，不然会出问题，因为版是0卡保存的，到时候都是加载到0卡
     dataloader, sampler = get_dataloader(type='CC3M',
                                 ######其实是yffcc15m数据集
                             batch_size=args.batch_size ,
@@ -96,18 +87,19 @@ def main():
                             dist_train=True,
                             use_lmdb=False)
     print("完成数据加载的构建")
+    # url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
+    # with dnnlib.util.open_url(url) as f:
+    #     vgg16 = torch.jit.load(f).eval().cuda()#####第二阶段就去掉这个
     #提前下载好
-    # model_path = '/mnt/data/user/lidehu/vae/ALIP/vgg16.pt'
-    # with dnnlib.util.open_url(model_path) as f:
-    #     vgg16 = torch.jit.load(f).eval().cuda()
+    model_path = '/mnt/data/user/lidehu/vae/ALIP/vgg16.pt'
+    with dnnlib.util.open_url(model_path) as f:
+        vgg16 = torch.jit.load(f).eval().cuda()
     #提前下载好
     global_step = GlobalStep()
     steps_per_epoch = args.train_num_samples // world_size // args.batch_size + 1
     steps_total = int(args.epochs * steps_per_epoch)
     print(steps_total)
     torch.distributed.barrier()
-    #########第二阶段
-   ########第二阶段
     opt = torch.optim.AdamW(
         params=[{"params": model_alip.parameters()}],#第一阶段的
         lr=args.lr, weight_decay=args.weight_decay, betas=(args.beta1, args.beta2))
@@ -132,8 +124,8 @@ def main():
     callback_func = SpeedCallBack(5, steps_total, args.batch_size)
     auto_scaler = torch.cuda.amp.grad_scaler.GradScaler(growth_interval=200)
     start_epoch = 0
-    reconstruction_criterionl2 = nn.MSELoss() 
-    criterion = nn.CrossEntropyLoss()
+    reconstruction_criterionl2 = nn.MSELoss()  
+    reconstruction_criterionl1 = nn.L1Loss() #####继续学习
     model_alip.train()
     torch.distributed.barrier()
      
@@ -142,15 +134,13 @@ def main():
         for _, img in enumerate(dataloader):
             img = img.cuda()
            #((12,3,256,256))
-            with torch.no_grad():
-                latent, _, [_, _, indices] = vq_model.encode(img)
-                #print(indices.shape)
-                b, _, h, w = latent.shape  # 获取批量大小和高度宽度，通道数在这里不重要
-                indices = indices.view(b, h, w)
-            reconstructed_images_index=model_alip(img)#bs,
-            
-            loss = criterion(reconstructed_images_index.view(-1,16384), indices.view(-1))
- #perc_loss+reconstruction_lossl2#
+            reconstructed_images,kl_loss=model_alip(img)#
+            reconstruction_lossl2 = reconstruction_criterionl2(img, reconstructed_images.float())
+            img_features = vgg16((img + 1) * (255/2), resize_images=False, return_lpips=True)
+            reconstructed_images_features = vgg16((reconstructed_images.float() + 1) * (255/2), resize_images=False, return_lpips=True)
+            perc_loss =(img_features - reconstructed_images_features).square().sum(1).mean()
+        ########感知损失使用L2_los,重建损失使用l1_loss
+            loss =  perc_loss+reconstruction_lossl2#
             auto_scaler.scale(loss).backward()
             if global_step.step % args.gradient_acc == 0:
                 auto_scaler.unscale_(opt)
@@ -162,11 +152,11 @@ def main():
             global_step.step += 1
 
             with torch.no_grad():
-                callback_func( lr_scheduler,float(loss), float(0), float(0), global_step.step)
+                callback_func( lr_scheduler,float(reconstruction_lossl2), float(perc_loss), float(0), global_step.step)
                 if summary_writer is not None:
                     #summary_writer.add_scalar(tag="unhit_count", scalar_value=unhit_count, global_step=global_step.step)
-                    summary_writer.add_scalar(tag="loss", scalar_value=loss.item(), global_step=global_step.step)
-                    #summary_writer.add_scalar(tag="percent_loss", scalar_value=perc_loss.item(), global_step=global_step.step)
+                    summary_writer.add_scalar(tag="lossl2", scalar_value=reconstruction_lossl2.item(), global_step=global_step.step)
+                    summary_writer.add_scalar(tag="percent_loss", scalar_value=perc_loss.item(), global_step=global_step.step)
                     #summary_writer.add_scalar(tag="reconstrution_loss", scalar_value=reconstruction_lossl1.item(), global_step=global_step.step)
                     summary_writer.add_scalar(tag="lr_backbone",
                                              scalar_value=lr_scheduler.get_last_lr()[0],
@@ -292,6 +282,11 @@ class SpeedCallBack(object):
             else:
                 self.init = True
                 self.tic = time.time()
+                
+
+if __name__ == "__main__":
+    main()
+
                 
 
 if __name__ == "__main__":
