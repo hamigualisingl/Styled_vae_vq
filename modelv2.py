295639ff41bad9d8febbd79ba8d633f4856ab5f9
dehuli <121349692+hamigualisingl@users.noreply.github.com>
2024-08-01 17:39:49 +0800

    Update modelv2.py

--- modelv2.py
+++ modelv2.py
@@ -379,8 +379,10 @@ class Styled_New_18Qvae_Block_stled(nn.Module):
             ("gelu", act_layer()),
             ("c_proj", nn.Linear(mlp_width, d_model))
         ]))
-        self.ln_1=FP32LayerNorm(d_model, norm_eps, norm_elementwise_affine)
+        self.ln_1=FP32LayerNorm(d_model, norm_eps, norm_elementwise_affine)#参与norm
         self.ln_2=FP32LayerNorm(d_model, norm_eps, norm_elementwise_affine)
+        self.ln_11=FP32LayerNorm(d_model, norm_eps, norm_elementwise_affine)#参与交互
+        self.ln_22=FP32LayerNorm(d_model, norm_eps, norm_elementwise_affine)
         self.norm2=AdaLayerNorm_stled(d_model, elementwise_affine=norm_elementwise_affine, eps=norm_eps)
         self.norm1 = AdaLayerNorm_stled(d_model, elementwise_affine=norm_elementwise_affine, eps=norm_eps)#进行自适应归一化
     def attention(self, x: torch.Tensor, attn_mask: Optional[torch.Tensor] = None):
@@ -388,12 +390,12 @@ class Styled_New_18Qvae_Block_stled(nn.Module):
        
     def forward(self, x: torch.Tensor,condation: torch.Tensor, index,attn_mask: Optional[torch.Tensor] = None):
         #condation的形状为2,bs，d_models,x形状为256，bs,d_models
-        x=torch.cat([condation[0:1],x], dim=0)
         norm_hidden_states = self.norm1(x,self.ln_1(condation[0]))
+        x=torch.cat([torch.zeros(1, *x.shape[1:], device=x.device, dtype=x.dtype),x], dim=0)##条件先norm，然后参与交互
+        norm_hidden_states=torch.cat([self.ln_11(condation[0:1]),norm_hidden_states], dim=0)#条件先norm，然后参与交互
         x = x + self.attn1(norm_hidden_states, norm_hidden_states, norm_hidden_states, need_weights=False, attn_mask=attn_mask)[0]#([516, 516]) torch.float32
-        x=torch.cat([condation[1:2],x], dim=0)
         x = x + self.mlp(self.norm2(x,self.ln_2(condation[1])))
-         
+        x=torch.cat([self.ln_22(condation[1:2]),x], dim=0)#
         return x
     
 
@@ -493,7 +495,7 @@ class VQVAE_Transformer_vit_sd3_hug_4096(nn.Module):
         ###############V2,这边是为了减轻量化损失影响的！
         mu_flattened = mu.view(-1, 128)
         similarity = cosine_similarity(mu_flattened.float().unsqueeze(1), self.V2.float(), dim=2)
-        similarity= similarity/torch.sum(similarity, dim=-1, keepdim=True)#线性加权，未使用softmax暂时没想好温控策略,这边也可以减少误差的影响,但是操作不当会增加
+        similarity= similarity/torch.sum(similarity, dim=-1, keepdim=True)#线性加权，未使用softmax暂时没想好温控策略,这边也可以减少误差的影响,但是操作不当会增加误差
         weighted_sum = self.ln_cosin(torch.matmul(similarity, self.V2))
         output = weighted_sum.view(mu.shape)
         ###############这边是为了减轻量化影响的, 保证含义是连续的,这样存在误差也无妨,本来就是总结图像，不指望他还原,目前追求还原质量是担心链路太长,每个环节都差一些,不好找原因
